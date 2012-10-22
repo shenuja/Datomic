c4898609f521df6dbd3be82c8bd095b27402241c
shenuja <shenuja@gmail.com>
2012-10-22 11:57:52 +0530

    checking in for subhash

--- src/noir_video/models/video.clj
+++ src/noir_video/models/video.clj
@@ -1,21 +1,26 @@
-(ns noir_video.models.video)
-(:require [datomic.api :as d]
+(ns noir_video.models.video
+  (:require [datomic.api :as d]
             [noir_video.models :as n]))
 
 (defn all-videos 
-  ([{
-    :title "Horsetail Falls"
+  ;(def videos (d/q '[:find ?v ?t :where [?v :video/title ?t]] (d/db n/conn)))
+  ;(map video-item videos)
+   ([{ :title "Horsetail Falls"
     :path "/lib/Horsetail Falls.mp4"
    },
-   {
-    :title "Yosemite Bears"
+   { :title "Yosemite Bears"
     :path "/lib/Yosemite Bears.mp4"
     },
-    {
-    :title "Yosemite Intro"
+    {:title "Yosemite Intro"
     :path "/lib/Yosemite Intro.mp4"
     }])
   )
+
+(defn video-item [item]
+  ({:id (get item :id)
+     :title (get item :title)
+     :path (str "/lib/" (get item :title) ".mp4" )
+    }))
 
 (defn get-by-name [name]
   )
